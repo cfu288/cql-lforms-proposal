a63253d4fb0cdaac02dd150642db799ce40f7f39
Christopher Fu <invent288cfu@gmail.com>
2024-04-09 11:45:55 -0400

    feat: update links for gh pages

--- web-demo/src/NavBar.tsx
+++ web-demo/src/NavBar.tsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 
 export function NavBar() {
-  const base = import.meta.env.BASE_URL;
+  // const base = import.meta.env.BASE_URL;
   return (
     <header>
       <nav>
         <ul>
           <li>
-            <Link to="/">Expressions</Link>
+            <Link to="/cql-lforms-proposal/">Expressions</Link>
           </li>
           <li>
-            <Link to="/libraries">Libraries</Link>
+            <Link to="/cql-lforms-proposal/libraries">Libraries</Link>
           </li>
           <li>
             <a href="https://github.com/cfu288/cql-lforms-proposal">GitHub</a>
