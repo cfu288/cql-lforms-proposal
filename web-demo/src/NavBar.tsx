e0b833300e1f2ff0ba78440d6d4ce5fe456cbfa1
Christopher Fu <invent288cfu@gmail.com>
2024-04-08 21:20:04 -0400

    feat: add link to github

--- web-demo/src/NavBar.tsx
+++ web-demo/src/NavBar.tsx
@@ -8,6 +8,7 @@ export function NavBar() {
           <li>
             <a href="/">Expressions</a>
             <a href="/Libraries">Libraries</a>
+            <a href="https://github.com/cfu288/cql-lforms-proposal">GitHub</a>
           </li>
         </ul>
       </nav>
