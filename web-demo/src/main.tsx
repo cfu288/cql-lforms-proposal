99c30587d8e86eec2db34bc6e853fd658e6c372f
Christopher Fu <invent288cfu@gmail.com>
2024-04-11 13:41:15 -0400

    feat: set basename in prod

--- web-demo/src/main.tsx
+++ web-demo/src/main.tsx
@@ -9,25 +9,31 @@ import QuestionnaireLibraryDemo from "./pages/QuestionnaireLibraryDemo.tsx";
 import { Home } from "./pages/Home.tsx";
 import { AppRoutes } from "./components/Routes";
 import QuestionnaireExpressionDemo from "./pages/QuestionnaireExpressionDemo.tsx";
+const basename = import.meta.env.DEV ? "/" : "/cql-lforms-proposal/";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: AppRoutes.home,
+      element: <Home />,
+    },
+    {
+      path: AppRoutes.expressions,
+      element: <ExpressionDemo />,
+    },
+    {
+      path: AppRoutes.questionnaireInlineExpressions,
+      element: <QuestionnaireExpressionDemo />,
+    },
+    {
+      path: AppRoutes.questionnaireLibraries,
+      element: <QuestionnaireLibraryDemo />,
+    },
+  ],
   {
-    path: AppRoutes.home,
-    element: <Home />,
-  },
-  {
-    path: AppRoutes.expressions,
-    element: <ExpressionDemo />,
-  },
-  {
-    path: AppRoutes.questionnaireInlineExpressions,
-    element: <QuestionnaireExpressionDemo />,
-  },
-  {
-    path: AppRoutes.questionnaireLibraries,
-    element: <QuestionnaireLibraryDemo />,
-  },
-]);
+    basename,
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
